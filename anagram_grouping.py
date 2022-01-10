32e1ac3f5668bd064d4f07ebfea920f5106435bd
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-01-10 12:36:44 +0530

    Create anagram_grouping.py

---
+++ anagram_grouping.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        dic = ({})
+        #dic = {} changes runtime 
+        for st in strs:
+            key = "".join(sorted(st))
+            if key in dic:
+                lst = dic[key]
+                lst.append(st)
+                dic[key] = lst
+            else:
+                lst = []
+                lst.append(st)
+                dic[key] = lst
+                
+        return dic.values() 
